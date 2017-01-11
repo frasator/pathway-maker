71a4b6ac5bfd848d87a685cc2d5e0bded36eb8ae
frasator <frasator@gmail.com>
2017-01-11 17:04:17 +0100

    New menu items, WIP.

--- conf/bioformats.js
+++ conf/bioformats.js
@@ -5,5 +5,12 @@ BIOFORMATS = {
         validator: 'Validator',
         hint: 'Plain text',
         url: ''
+    },
+    "SIMPLE_INTERACTION_FILE": {
+        value: 'SIMPLE_INTERACTION_FILE',
+        text: 'Simple interaction file',
+        validator: 'Validator',
+        hint: 'Plain text',
+        url: ''
     }
 };
