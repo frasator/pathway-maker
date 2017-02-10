fff24c54c2118fc7d39522173cbbf0f3fd6a8d80
frasator <frasator@gmail.com>
2017-02-10 11:20:25 +0100

    Minor fixes, bower and package files updates.

--- conf/config.js
+++ conf/config.js
@@ -4,7 +4,8 @@
  **/
 STEVIA_MANAGER_LOG = true;
 STEVIA_SERVER_HOST = 'http://fsalavert:5555';
+// STEVIA_SERVER_HOST = 'http://ws.babelomics.org/stv01';
 
-STEVIA_SHOW_JOB_ERROR_FILES = true;
+STEVIA_SHOW_JOB_ERROR_FILES = false;
 UPLOAD_FILES_WITH_ERRORS = true;
-STEVIA_SHOW_HIDDEN_FILES = true;
+STEVIA_SHOW_HIDDEN_FILES = false;
